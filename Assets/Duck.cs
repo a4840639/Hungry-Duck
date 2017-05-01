1be5c80aa795e744f8e7e0c3d9e6f3417efff8b0
Ericzhang1 <zhangzicong.passion@gmail.com>
2017-05-01 00:56:37 -0400

    sound

--- Assets/Duck.cs
+++ Assets/Duck.cs
@@ -11,35 +11,55 @@ public class Duck : MonoBehaviour {
 	Vector3 direciton = Vector3.forward;
 	CharacterController controller;
 	Vector3 lastPos;
+    public AudioClip eatsound;
+    public AudioClip swimsound;
+    public AudioClip boostsound;
+    public AudioClip bouncesound;
+    private AudioSource source;
+    private float vollowrange = .3f;
+    private float volhighrange = .6f;
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController>();
+        source = GetComponent<AudioSource>(); 
 	}
-
+     
 	// Update is called once per frame
 	void Update () {
 		Vector3  moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
 		float moveMagnitude = moveDirection.magnitude;
 		if (moveMagnitude > 1) {
 			moveDirection.Normalize ();
 			moveMagnitude = 1f;
 		}
 		float dash = Input.GetAxisRaw ("Fire1");
-
+        if (Input.GetKeyDown("j")){
+            source.PlayOneShot(boostsound, 1f);
+        }
+        if (((Input.GetKeyDown("w"))&& (!Input.GetKeyDown("s")) && (!Input.GetKeyDown("a")) && (!Input.GetKeyDown("d")))||
+                ((!Input.GetKeyDown("w")) && (Input.GetKeyDown("s")) && (!Input.GetKeyDown("a")) && (!Input.GetKeyDown("d")))||
+                ((!Input.GetKeyDown("w")) && (!Input.GetKeyDown("s")) && (Input.GetKeyDown("a")) && (!Input.GetKeyDown("d")))||
+                ((!Input.GetKeyDown("w")) && (!Input.GetKeyDown("s")) && (!Input.GetKeyDown("a")) && (Input.GetKeyDown("d"))))
+        {
+            float vol = Random.Range(vollowrange, volhighrange);
+            source.PlayOneShot(swimsound, vol);
+        }
 		if (dashtime > 0) {
-			if (dashtime > 0.1f) {
+            if (dashtime > 0.1f) {
 				velocity = Vector3.SmoothDamp (velocity, velocity * 0.2f, ref accel, 0.3f);
 			}
-			dashtime -= Time.deltaTime;
+            dashtime -= Time.deltaTime;
 		} else if (dash == 0) {
-			velocity = moveDirection * Time.deltaTime * speed;
-		}
+            velocity = moveDirection * Time.deltaTime * speed;
+            
+        }
 		else {
 			velocity = direciton * Time.deltaTime * speed * 3f;
 			accel = velocity * 0.9f - velocity;
 			dashtime = 0.4f;
-		}
+        }
 
 		controller.Move (velocity);
 		if (moveDirection.sqrMagnitude > 0) {
@@ -55,7 +75,8 @@ public class Duck : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Food"))
 		{
 			other.gameObject.SetActive (false);
-		}
+            source.PlayOneShot(eatsound, 1F);
+        }
 	}
 
 	void OnControllerColliderHit()
@@ -65,6 +86,7 @@ public class Duck : MonoBehaviour {
 			velocity = -0.6f * velocity;
 			controller.Move (velocity);
 			accel = velocity * 0.9f - velocity;
-		}
+            source.PlayOneShot(bouncesound, 1f);
+        }
 	}
 }
